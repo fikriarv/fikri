46d0724c4bf8fb9f021aaf622acc8cb3aac59de3
fikriarv <fkri.adiansyah@icloud.com>
2023-11-07 08:37:43 +0700

    commit3

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,35 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Head from "next/head";
+import Image from "next/image";
 
-const About = () => {
+const About = ({ data }) => {
   return (
     <>
-    <Navbar/>
-    <h1>About Fikri</h1>
+      <Head>
+        <title>About | Belajar Next JS</title>
+        <meta name="description" content="Belajar Next JS" />
+      </Head>
+      <Navbar />
+      <Image src="/vercel.svg" alt="Logo Vercel" width={300} height={200} />
+      <h1>About Fikri</h1>
+      {data.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+        </div>
+      ))}
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      data, // Pass the data as a prop
+    },
+  };
 };
 
 export default About;
